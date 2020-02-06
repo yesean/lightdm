0b5163f57cf63b07640cab492e2b48ac11367a22
Eric Cheng <ericcheng986@gmail.com>
2020-02-06 01:48:24 -0500

    fixed bad merge

--- src/components/SelectItem.vue
+++ src/components/SelectItem.vue
@@ -18,11 +18,9 @@
 
 <script>
     import { avatar } from '@/settings';
-
     export default {
         name: 'select-item',
         props: ['mode', 'item', 'selected', 'noicon'],
-
         methods: {
             select() {
                 this.$emit('select');
@@ -31,11 +29,9 @@
                 if (this.mode === 'user') {
                     return avatar(this.item.image);
                 }
-
                 if (this.mode === 'desktop') {
                     let key = this.item.key.toLowerCase();
                     let icon;
-
                     if (key.indexOf('plasma') > -1 || key.indexOf('kde') > -1) {
                         icon = 'kde';
                     } if (key.indexOf('gnome') > -1) {
@@ -67,67 +63,14 @@
                     } if (key.indexOf('awesome') > -1) {
                         icon = 'awesome';
                     }
-
                     if (!icon) {
                         return '';
                     }
-
                     return require('../assets/images/desktops/' + icon + '.png');
                 }
             }
         }
-        if (key.indexOf("gnome") > -1) {
-          icon = "gnome";
-        }
-        if (key.indexOf("xfce") > -1) {
-          icon = "xfce";
-        }
-        if (key.indexOf("unity") > -1 || key.indexOf("ubuntu") > -1) {
-          icon = "ubuntu";
-        }
-        if (key.indexOf("dde") > -1 || key.indexOf("deepin") > -1) {
-          icon = "deepin";
-        }
-        if (key.indexOf("lxde") > -1 || key.indexOf("lxqt") > -1) {
-          icon = "lxde";
-        }
-        if (key.indexOf("pantheon") > -1 || key.indexOf("elementary") > -1) {
-          icon = "elementary";
-        }
-        if (key.indexOf("mate") > -1) {
-          icon = "mate";
-        }
-        if (key.indexOf("cinnamon") > -1 || key.indexOf("mint") > -1) {
-          icon = "cinnamon";
-        }
-        if (
-          key.indexOf("enlightenment") > -1 ||
-          (key.length === 3 && /e[1-9]{2}/g.test(key))
-        ) {
-          icon = "enlightenment";
-        }
-        if (key.indexOf("liri") > -1) {
-          icon = "liri";
-        }
-        if (key.indexOf("i3") > -1) {
-          icon = "i3";
-        }
-        if (key.indexOf("xmonad") > -1) {
-          icon = "xmonad";
-        }
-        if (key.indexOf("budgie") > -1 || key.indexOf("solus") > -1) {
-          icon = "budgie";
-        }
-
-        if (!icon) {
-          return "";
-        }
-
-        return require("../assets/images/desktops/" + icon + ".png");
-      }
-    }
-  }
-};
+    };
 </script>
 
 <style lang="scss" scoped>
